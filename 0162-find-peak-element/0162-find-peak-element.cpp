3ff7a97557b79dd271a4957100dc11fc6d928b17
charvikhandelwal52-dotcom <charvikhandelwal52@gmail.com>
2026-08-11 19:16:39 +0530

    Time: 0 ms (100%), Space: 12.5 MB (73.86%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -16,7 +16,7 @@ public:
                 low= mid+1;
             }
             else if(nums[mid]> nums[mid+1]) high= mid-1;
-            else low= mid+1;
+            else high= mid-1;
         }
         return -1;
     }
