ea208743447e666bc226c60dce1c761c918510f5
charvikhandelwal52-dotcom <charvikhandelwal52@gmail.com>
2026-08-11 19:16:19 +0530

    Time: 0 ms (100%), Space: 12.5 MB (73.86%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -15,7 +15,8 @@ public:
             else if(nums[mid]> nums[mid-1]){
                 low= mid+1;
             }
-            else high= mid-1;
+            else if(nums[mid]> nums[mid+1]) high= mid-1;
+            else low= mid+1;
         }
         return -1;
     }
